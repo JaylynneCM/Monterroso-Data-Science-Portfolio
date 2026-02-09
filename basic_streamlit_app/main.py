0d9a75a8c5c83128040761045648a4f070d6231c
JaylynneCM <jcalder5@nd.edu>
2026-02-08 23:53:07 -0500

    Updated Title and Image

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,20 +1,23 @@
 import streamlit as st
 import pandas as pd
-st.title("Hello, Streamlit!")
  
-#Title for project
-st.title("Online Sales Analysis")
+ #Title for Project
+st.title("💻 Online Sales Analysis 💻")
 
+# Image for Project 
+st.image(
+    "basic_streamlit_app/Images/shutter-speed-BQ9usyzHx_w-unsplash.jpg",
+    width=400
+)
 
-# Load the CSV file
+col1, col2  = st.columns(2)
+
+with col1:
+        
+        st.write(' Photo by Shutter Speed on Unsplash')
+    
+# Load CSV data file
 df = pd.read_csv("basic_streamlit_app/data/Online_Sales_Data.csv")
 
 st.write("Here's our data:")
 st.dataframe(df)
-
-#Filter by Region
-Region = st.selectbox("Select a Region", df["Region"].unique())
-filtered_df = df[df["Region"] == Region]
-
-st.write(f"Sales in {Region}:")
-st.dataframe(filtered_df)
